2fa1e4ef50e291fb2d7357d24ef6a8945624467a
Sahil Nayak <76465898+sahilnayak7702@users.noreply.github.com>
2024-12-23 13:47:36 +0530

    Update main.py

--- local_env/main.py
+++ local_env/main.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="local_env\config.yaml", epochs=1)  # train the model
+results = model.train(data="path/to/config.yaml", epochs=50)  # train the model
